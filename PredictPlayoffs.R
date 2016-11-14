1a0ba89de70279575dd9612185f70f5722fa3f28
jackoliverwerner <jack.oliver.werner@gmail.com>
2016-11-14 16:25:26 -0600

    Update PredictPlayoffs.R

--- PredictPlayoffs.R
+++ PredictPlayoffs.R
@@ -4,8 +4,8 @@
 
 library(skellam)
 
-setwd("/Users/jackwerner/Documents/My Stuff/Baseball/Team Rating System")
-#setwd("C:/Users/jack.werner1/Documents/BB")
+#setwd("/Users/jackwerner/Documents/My Stuff/Baseball/Team Rating System")
+setwd("C:/Users/jack.werner1/Documents/BB")
 
 source("getSeasonResults.R")
 source("JWPitchers.R")
@@ -17,7 +17,7 @@ MLBteams <- paste0(getwd(), "/MLBteams.csv")
 # Get game results #
 ####################
 
-year <- 2016
+year <- 2003
 
 season.results <- getLeagueResults(year, MLBteams, includePlayoffs = T)
 
@@ -89,6 +89,8 @@ pitcher.scores.final[is.na(pitcher.scores.final$score),]$score <- pitcher.scores
 # Prediction data frame -- JW #
 ###############################
 
+hfa <- 1.17
+
 playoff.preds.JW <- playoffs.df %>% 
   left_join(team.scores, by = c("home.team" = "team")) %>% rename(home.offense = score) %>%
   left_join(team.scores, by = c("away.team" = "team")) %>% rename(away.offense = score) %>%
@@ -99,13 +101,13 @@ playoff.preds.JW <- playoffs.df %>%
          away.raw = pskellam(-1, lambda1 = home.lambda, lambda2 = away.lambda),
          home.percent.JW = home.raw/(home.raw + away.raw), 
          away.percent.JW = away.raw/(away.raw + home.raw),
-         home.percent.JW.adj = 1.17*home.percent.JW/(2.17 - home.percent.JW),
-         away.percent.JW.adj = (away.percent.JW/1.17)/(1/1.17 + 1 - away.percent.JW),
+         home.percent.JW.adj = hfa*home.percent.JW/((hfa - 1)*home.percent.JW + 1),
+         away.percent.JW.adj = (away.percent.JW/hfa)/((1/hfa - 1)*away.percent.JW + 1),
          su = home.percent.JW.adj + away.percent.JW.adj)
 
-playoff.preds <- playoff.preds.JW %>% 
+playoff.preds.pre <- playoff.preds.JW %>% 
   select(home.team, home.starter, away.team, away.starter, home.R, away.R, winner,
-         home.percent.JW)
+         home.percent.JW, home.percent.JW.adj)
 
 #################################################
 # Simple prediction data frame (with season W%) #
@@ -117,13 +119,81 @@ teamWPs <- reg.results %>% group_by(team) %>%
 playoff.preds.WP <- playoffs.df %>%
   left_join(teamWPs, by = c("home.team" = "team")) %>% rename(WP.home = WP) %>%
   left_join(teamWPs, by = c("away.team" = "team")) %>% rename(WP.away = WP) %>%
-  mutate(home.percent.WP = WP.home/(WP.home + WP.away),
-         away.percent.WP = WP.away/(WP.home + WP.away))
+  mutate(#home.percent.WP = WP.home/(WP.home + WP.away),
+         #away.percent.WP = WP.away/(WP.home + WP.away),
+         home.percent.WP = (WP.home - WP.away + 1)/2,
+         away.percent.WP = (WP.away - WP.home + 1)/2,
+         home.percent.WP.adj = hfa*home.percent.WP/((hfa - 1)*home.percent.WP + 1),
+         away.percent.WP.adj = (away.percent.WP/hfa)/((1/hfa - 1)*away.percent.WP + 1))
+
+playoff.preds <- cbind(playoff.preds.pre, select(playoff.preds.WP, home.percent.WP, home.percent.WP.adj)) %>%
+  mutate(home.won = winner == home.team)
 
 
+########################
+# Evaluate predictions #
+########################
+
+# Confusion matrices
+(conf.WP <- table(playoff.preds$home.won, playoff.preds$home.percent.WP > .5))
+(conf.WP.adj <- table(playoff.preds$home.won, playoff.preds$home.percent.WP.adj > .5))
+(conf.JW <- table(playoff.preds$home.won, playoff.preds$home.percent.JW > .5))
+(conf.JW.adj <- table(playoff.preds$home.won, playoff.preds$home.percent.JW.adj > .5))
+
+# Error rates
+sum(diag(conf.WP))/sum(conf.WP)
+sum(diag(conf.WP.adj))/sum(conf.WP.adj)
+sum(diag(conf.JW))/sum(conf.JW)
+sum(diag(conf.JW.adj))/sum(conf.JW.adj)
+
+# Brier scores
+(brier.WP <- mean((playoff.preds$home.percent.WP - playoff.preds$home.won)^2))
+(brier.WP.adj <- mean((playoff.preds$home.percent.WP.adj - playoff.preds$home.won)^2))
+(brier.JW <- mean((playoff.preds$home.percent.JW - playoff.preds$home.won)^2))
+(brier.JW.adj <- mean((playoff.preds$home.percent.JW.adj - playoff.preds$home.won)^2))
+
+# Distrimination slope
+playoff.preds %>% group_by(home.won) %>%
+  summarize(home.percent.WP = mean(home.percent.WP), home.percent.WP.adj = mean(home.percent.WP.adj),
+            home.percent.JW = mean(home.percent.JW), home.percent.JW.adj = mean(home.percent.JW.adj))
+
+# ROC curve -- Messed Up
+truePositiveRate <- function(thresh, pred.vec, actual.vec) {
+  tp <- sum(actual.vec[pred.vec > thresh])
+  p <- sum(actual.vec)
+  
+  return(tp/p)
+}
+
+falsePositiveRate <- function(thresh, pred.vec, actual.vec) {
+  tp <- sum(actual.vec[pred.vec > thresh])
+  p <- sum(actual.vec)
+  
+  fp <- length(actual.vec[pred.vec > thresh]) - tp
+  n <- length(actual.vec) - p
+  
+  return(fp/n)
+}
+
+it <- seq(1, 0, by = -.01)
+
+roc <- data.frame(thresh = it,
+                  tpr.WP = sapply(it, truePositiveRate, playoff.preds$home.percent.WP, playoff.preds$home.won),
+                  fpr.WP = sapply(it, falsePositiveRate, playoff.preds$home.percent.WP, playoff.preds$home.won),
+                  tpr.WP.adj = sapply(it, truePositiveRate, playoff.preds$home.percent.WP.adj, playoff.preds$home.won),
+                  fpr.WP.adj = sapply(it, falsePositiveRate, playoff.preds$home.percent.WP.adj, playoff.preds$home.won),
+                  tpr.JW = sapply(it, truePositiveRate, playoff.preds$home.percent.JW, playoff.preds$home.won),
+                  fpr.JW = sapply(it, falsePositiveRate, playoff.preds$home.percent.JW, playoff.preds$home.won),
+                  tpr.JW.adj = sapply(it, truePositiveRate, playoff.preds$home.percent.JW.adj, playoff.preds$home.won),
+                  fpr.JW.adj = sapply(it, falsePositiveRate, playoff.preds$home.percent.JW.adj, playoff.preds$home.won))
+
+ggplot(data = roc, aes(x = fpr.WP, y = tpr.WP)) + geom_line(color = "blue") + 
+  geom_abline(slope = 1, intercept = 0, linetype = 2) +
+  geom_line(aes(x = fpr.WP, y = tpr.WP.adj), color = "red")
+
+ggplot(data = roc, aes(x = fpr.JW.adj, y = tpr.JW.adj)) + geom_line(color = "blue") + 
+  geom_abline(slope = 1, intercept = 0, linetype = 2)
 
 
-
-
-
+ggplot(data = roc, aes(x = thresh, y = fpr.JW)) + geom_line()
 
